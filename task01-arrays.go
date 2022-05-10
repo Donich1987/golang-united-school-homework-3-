b97f62734dd4ee3b9b62138a5c32c0a65c4a2ab8
Donich1987 <99492348+Donich1987@users.noreply.github.com>
2022-05-11 00:55:27 +0300

    Update task01-arrays.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,10 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	for i := 0; i <= len(input); i++ {
+	for i := 0; i < len(input); i++ {
 		result += input[i]
 	}
 	z := float32(len(input))
-	result = result / z
+	result = result/z
 	return result
 }
